1933a1f31a0f8f5275ff9f7476f7641c15b6fd5b
peacemaker <benedict.risdyanto@gmail.com>
2016-02-24 15:56:02 +0700

    pemahaman pemrograman di backend

---
+++ backend/backend.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+application = Flask(__name__)
+
+@application.route("/login_json/<user>/<password>")
+def login_json(user,password):
+    print(user,password)
+    if user == 'risdyanto' and password == 'abc123':
+        hasil = {}
+        hasil['login_status'] = 'User diterima'
+        hasil['login_sukses'] = True
+        return jsonify(hasil)
+    else:
+        hasil = {}
+        hasil['login_status'] = 'User ditolak'
+        hasil['login_sukses'] = False
+        return jsonify(hasil)
+
+if __name__ == '__main__':
+    application.run(port=8080)
