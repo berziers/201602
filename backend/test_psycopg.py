cb6ff5c41bc1b80502c25b2475bb56c3500ca170
Eko Wibowo <swdev.bali@gmail.com>
2016-02-25 01:44:49 +0700

    just crud login etc

---
+++ backend/test_psycopg.py
@@ -0,0 +1,28 @@
+import psycopg2 as dbapi2
+
+def check_login(username, password):
+    query = "select id, username from users where username='{}' and password='{}'".format(username, password)
+    cur.execute(query)
+    rows = cur.fetchall()
+    return len(rows) > 0
+
+db = dbapi2.connect (database="dafambackend", user="ekowibowo", password="")
+cur = db.cursor()
+
+#clear data
+cur.execute ("DELETE FROM users")
+
+#insert data
+insert = "insert into users(username, password) values('{}','{}')"
+cur.execute(insert.format('eko', 'rahasia'))
+cur.execute(insert.format('ela', 'jugarahasia'))
+
+print(check_login('eko', 'rahzasia'))
+
+cur.execute ("SELECT * FROM users");
+rows = cur.fetchall()
+for i, row in enumerate(rows):
+    print("Row", i, "value = ", row)
+
+
+
